7461497ad605027945eb4e195e0e9adc7064b4ca
Leena Monthero <leenamonthero@gmail.com>
2023-11-05 13:44:29 +0900

    code updated

---
+++ app.py
@@ -0,0 +1,21 @@
+from src.DimondPricePrediction.pipelines.prediction_pipeline import CustomData,PredictPipeline
+from flask import Flask,request,render_template,jsonify
+
+app=Flask(__name__)
+
+
+@app.route('/')
+def home_page():
+    return render_template("index.html")
+
+@app.route('/predict',methods=["GET","POST"])
+def predict_datapoints():
+    if request.method=="GET":
+        return render_template("from.html")
+    
+    else:
+        
+        pass
+
+
+app.run()
